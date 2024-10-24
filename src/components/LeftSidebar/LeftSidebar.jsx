6af786b4e76bbea2028f3f27af3b9209afe50910
Amlan <sahuamlanranjan@gmail.com>
2024-10-24 22:00:35 +0530

    "Updated CSS and JSX files for ChatBox and LeftSidebar components, adding styles and elements for chat user interface and left sidebar navigation."

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -7,8 +7,26 @@ const LeftSidebar = () => {
     <div className='ls'>
        <div className="ls-top">
          <div className="ls-nav">
-             <img src={assets.logo} classname='' alt="" />
+             <img src={assets.logo} classname='logo' alt="" />
+             <div className="menu">
+                <img src={assets.menu_icon} alt="" />
+             </div>
          </div>
+         <div className="ls-search">
+             <img src={assets.search_icon} alt="" />
+             <input type="text" placeholder='Search here...' />
+         </div>
+       </div>
+       <div className="ls-list">
+          {Array(12).fill("").map((item,index)=>(
+            <div key={index} className="friends">
+            <img src={assets.profile_img} alt="" />
+            <div>
+               <p>Richard Sanford</p>
+               <span>Hello, How are you?</span>
+            </div>
+        </div>
+          ))}
        </div>
     </div>
   )
