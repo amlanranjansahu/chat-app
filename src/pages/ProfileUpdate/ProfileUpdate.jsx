425852d31665be2a5f8d8844043bbd90f9c54264
Amlan <sahuamlanranjan@gmail.com>
2024-10-30 20:17:22 +0530

    "Refactored upload.js to return a Promise and updated ProfileUpdate.jsx to handle image upload and profile updates"

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -3,17 +3,49 @@ import './ProfileUpdate.css'
 import assets from '../../assets/assets'
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth, db } from '../../config/firebase';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import upload from '../../lib/upload';
 
 const ProfileUpdate = () => {
     
-    const navigate = useNavigate
+    const navigate = useNavigate();
     const [image,setImage] = useState(false);
     const [name,setName] = useState("");
     const [bio,setBio] = useState("");
     const [uid,setUid] = useState("");
-    const [prevImage,setPrevImage] = useState("");
+    const [prevImage,setPrevImage] = useState("")
+
+
+    const profileUpdate = async (event) => {
+       event.preventDefault();
+       try {
+        
+        if (!prevImage && !image) {
+           toast.error("Upload profile picture")
+        }
+         const docRef = doc(db,'users',uid);
+         if (image) {
+           const imgUrl = await upload(image);
+           setPrevImage(imgUrl);
+           await updateDoc(docRef,{
+              avatar:imgUrl,
+              bio:bio,
+              name:name
+           })
+         }
+         else{
+           await updateDoc(docRef,{
+              bio:bio,
+              name:name
+           })
+         }
+       } catch (error) {
+        
+       }
+    }
+
 
 
     useEffect(()=>{
@@ -42,7 +74,7 @@ const ProfileUpdate = () => {
   return (
     <div className='profile'>
        <div className="profile-container">
-         <form>
+         <form onSubmit={profileUpdate}>
             <h3>Profile Details</h3>
             <label htmlFor="avatar">
               <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
